4608de8cb0101390237ec6a63376702ce731f0a8
Javier Palomo Almena <javier.palomo.almena@gmail.com>
2015-02-28 23:33:43 +0100

    Install 'rbenv': - Several plugins - Include the rbenv config in the Zsh config

--- ubuntu/install.sh
+++ ubuntu/install.sh
@@ -8,3 +8,15 @@ sudo apt-get update
 
 echo "Install packages"
 sudo apt-get install -y $(cat packages)
+
+# rbenv (+ plugins)
+echo "Installing rbenv (+ plugins)"
+curl https://raw.githubusercontent.com/fesplugas/rbenv-installer/master/bin/rbenv-installer | bash
+
+if [ ! $(which rbenv) ]; then
+  echo "Adding rbenv config"
+  mkdir -p ~/.zshrc.d
+  cat zshrc.d/env_rbenv.sh | tee ~/.zshrc.d/01_rbenv.sh
+  # This source only works in the scope of the script, outside we need to reload
+  . ~/.zshrc.d/01_rbenv.sh
+fi
